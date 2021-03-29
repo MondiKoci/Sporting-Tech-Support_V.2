7b70550eb424e7439fd7f38078f100372f826ec5
Mondi Koci <101282062@georgebrown.ca>
2021-03-28 21:56:48 -0400

    Cache updates (2)

---
+++ Models/IncidentViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GBCSporting2021_TheDevelopers.Models
+{
+    public class IncidentViewModel
+    {
+        public List<Customer> Customers { get; set; }
+        public List<Product> Products { get; set; }
+        public List<Technician> Technicians { get; set; }
+        public Incident currentIncident { get; set; }
+        public string Page { get; set; }
+    }
+}
