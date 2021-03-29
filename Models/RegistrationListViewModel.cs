f991ceebfd263d07d0e82560ededa949d502adc3
Paolo <74078018+paoloreus@users.noreply.github.com>
2021-03-28 22:48:26 -0400

    Add files via upload

---
+++ Models/RegistrationListViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GBCSporting2021_TheDevelopers.Models
+{
+    public class RegistrationListViewModel
+    {
+        public List<Registration> Registrations { get; set; }
+        public List<Customer> Customers { get; set; }
+        public List<SelectListItem> CustomerList { get; set; }
+        public List<SelectListItem> ProductList { get; set; }
+        public List<Product> Products { get; set; }
+
+        public Customer Customer { get; set; }
+
+       
+    }
+}
