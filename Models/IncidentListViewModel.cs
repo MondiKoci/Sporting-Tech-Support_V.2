414306140eb9410cedee9d08e7a7054eb067b606
Paolo <74078018+paoloreus@users.noreply.github.com>
2021-04-05 21:59:08 -0400

    Add files via upload

---
+++ Models/IncidentListViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GBCSporting2021_TheDevelopers.Models
+{
+    public class IncidentListViewModel : IncidentViewModel
+    {
+        public List<Incident> Incidents { get; set; }
+        public Technician Technician { get; set; }
+    }
+}
--- Models/IncidentViewModel.cs
+++ Models/IncidentViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,10 +8,13 @@ namespace GBCSporting2021_TheDevelopers.Models
 {
     public class IncidentViewModel
     {
+        public Incident Incident { get; set; }
         public List<Customer> Customers { get; set; }
+        public List<SelectListItem> CustomerList { get; set; }
+        public List<SelectListItem> ProductList { get; set; }
+        public List<SelectListItem> TechnicianList { get; set; }
         public List<Product> Products { get; set; }
         public List<Technician> Technicians { get; set; }
-        public Incident currentIncident { get; set; }
-        public string Page { get; set; }
+        public string ActionPage { get; set; }
     }
 }
