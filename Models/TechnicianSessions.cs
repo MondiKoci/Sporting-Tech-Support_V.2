692b6ea7c4e391728f3511cb2d22b9ecb57841f9
Paolo <74078018+paoloreus@users.noreply.github.com>
2021-04-07 18:21:34 -0400

    Add files via upload
    Session Handler for Technicians

---
+++ Models/TechnicianSessions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace GBCSporting2021_TheDevelopers.Models
+{
+    public class TechnicianSessions
+    {
+        private const string TechniciansKey = "myTechnicians";
+
+        private ISession session { get; set; }
+        public TechnicianSessions(ISession session)
+        {
+            this.session = session;
+        }
+
+        public void setTechnician(Technician technician)
+        {
+            session.SetObject(TechniciansKey, technician);
+        }
+        public Technician GetTechnician()
+        {
+            return session.GetObject<Technician>(TechniciansKey) ?? new Technician();
+        }
+
+        public void RemoveTechnician()
+        {
+            session.Remove(TechniciansKey);
+        }
+    }
+}
