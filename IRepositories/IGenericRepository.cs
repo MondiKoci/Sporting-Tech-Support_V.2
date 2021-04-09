95493c651afc676548e49b9800a995b41a891e73
MondiKoci <mondi.s.koci@gmail.com>
2021-04-08 22:26:38 -0400

    Repository folders added

---
+++ IRepositories/IGenericRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GBCSporting2021_TheDevelopers.IRepositories
+{
+    public class IGenericRepository
+    {
+    }
+}
